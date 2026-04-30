47b270190f7a716284081ff682519c67775b3337
Mubashar <mubashar.ali.online@gmail.com>
2026-04-30 22:10:04 +0530

    fix: move eslint bypass to eslint.config.mjs to support Next.js 16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,18 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react-hooks/immutability": "off",
+      "no-use-before-define": "off",
+      "@typescript-eslint/no-use-before-define": "off"
+    }
+  }
 ]);
 
 export default eslintConfig;
